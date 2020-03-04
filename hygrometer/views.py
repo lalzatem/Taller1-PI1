057dac797bd51754cbd04041fef9043a34466860
lalzatem <37983508+lalzatem@users.noreply.github.com>
2020-03-04 16:04:51 -0500

    Actualizacion1.0

---
+++ hygrometer/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Hygrometer
+from .serializers import HygrometerSerializer
+
+class HygrometerViewSet(viewsets.ModelViewSet):
+    queryset = Hygrometer.objects.all().order_by('-created')
+    serializer_class = HygrometerSerializer
